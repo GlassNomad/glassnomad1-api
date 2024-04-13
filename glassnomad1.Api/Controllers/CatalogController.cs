911f3e87e233603a28e73d7ec17742c1560d6a12
rodwanmohamed <mohamed.548@buckeyemail.osu.edu>
2024-04-13 08:28:44 -0400

    pt2

--- glassnomad1.Api/Controllers/CatalogController.cs
+++ glassnomad1.Api/Controllers/CatalogController.cs
@@ -6,9 +6,12 @@ namespace glassnomad1.Api.Controllers {
 	[Route("[controller]")]
 	public class CatalogController: ControllerBase{
         [HttpGet]
-        public IActionResult GetItems()
-        {
-            return Ok("Hello World!");
+        public IActionResult GetItems(){
+            var items =new List<Ites>() {
+                new Items("Shirt", "Ohio State Shirt", "Nike", 29.99mm),
+                new Items("Shorts", "Ohio State Shorts", "Nike", 49.99mm),
+            }
+            return Ok(items);
         }
     }
 }
